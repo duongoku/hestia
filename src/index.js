8242b124e7eb594825f41218621c5ce1db1814b8
duongoku <nguyenvubinhduong@gmail.com>
2019-01-04 15:44:06 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import 'typeface-roboto';
 import * as serviceWorker from './serviceWorker';
 import GlobalStatusBar from './globalStatusBar/globalStatusBar.js'
+import Scoreboard from './scoreboard/scoreboard.js'
 import Sidenav from './sidenav/sidenav.js';
 import { Button } from '@material-ui/core';
 
@@ -27,6 +28,24 @@ class Hestia extends React.Component {
                 })} pages={[
                     <Button onClick={() => alert(1)}>Alert (1)</Button>
                 ]} />
+                
+                <Scoreboard obj={{
+
+                    problem_code: ['A1','B2','C3'],
+
+                    data: [
+                        {handle: 'GHTH01', penalty:40000, A1: 70, B2: 100, C3: 85, subs: [3,2,2], total: -255},
+                        {handle: 'GHTH02', penalty:50000, A1: 100, B2: 50, C3: 50, subs: [1,2,2], total: -200},
+                        {handle: 'GHTH03', penalty:30000, A1: 0, B2: 100, C3: 100, subs: [0,2,3], total: -200},
+                    ],
+
+                    columns: [
+                        {Header: 'Handle', accessor: 'handle'},
+                        {Header: 'Penalty', accessor: 'penalty'},
+                        {Header: 'Total', accessor: 'total'},
+                    ],
+
+                }}/>
             </>
         )
     }
